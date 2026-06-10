2d7fa2e75fadc4695332b8fc4c4d4e4192e37c3b
ikhlas-ct <ikhlasulamalmawardi@gmail.com>
2026-06-10 14:59:06 +0700

    save

--- resources/views/partials/user/footer.blade.php
+++ resources/views/partials/user/footer.blade.php
@@ -1,13 +1,9 @@
-    <div class="container-fluid d-flex justify-content-between">
-      <nav class="pull-left">
-        <ul class="nav">
-          <li class="nav-item">
-            2024, made with <i class="fa fa-heart heart text-danger"></i> by {{ $settings->nama_kecamatan }}
-
-
-    
-        </ul>
-      </nav>
-
-
-    </div>
+<div class="container-fluid d-flex justify-content-between">
+  <nav class="pull-left">
+    <ul class="nav">
+      <li class="nav-item">
+        {{ date('Y') }}, made </i> by {{ $settings->nama_kecamatan }}
+      </li>
+    </ul>
+  </nav>
+</div>
